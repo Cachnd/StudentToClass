3a30f4a83174d154e795335e1f410dacb081eb57
Alex <alex.cachnd@gmail.com>
2021-05-04 08:47:20 -0400

    Initial Commit

---
+++ src/main/java/com/stc/demo/StcApplication.java
@@ -0,0 +1,15 @@
+package com.stc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcApplication.class, args);
+	}
+
+
+
+}
