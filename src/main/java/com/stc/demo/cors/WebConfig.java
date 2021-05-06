b940982ab17c739181932c14e468d734ad4cab70
Alex Cardona <alex.cardona@bo.truextend.com>
2021-05-06 11:38:31 -0400

    Fixed WebConfig blocking Swagger-ui url

--- src/main/java/com/stc/demo/cors/WebConfig.java
+++ src/main/java/com/stc/demo/cors/WebConfig.java
@@ -3,6 +3,7 @@ package com.stc.demo.cors;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
